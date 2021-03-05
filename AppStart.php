e14856065b766ad4f7a36cb46929658a36256956
codelac <70864290+codelac@users.noreply.github.com>
2021-03-05 16:57:37 +0100

    Create AppStart.php

---
+++ AppStart.php
@@ -0,0 +1,3 @@
+{"deviceNo":"69520","deviceHash":"8a8780843d3bc5a4937588fdc1d7b8620c6d896b","updateMessage":"","updatePackageName":"","updateServer":"https:\/\/play.google.com\/store\/","updateStatus":false,"adsPeriod":"2","adsTimeout":"120","adMobBannerAds":false,"adMobInterstitialAds":false,
+
+"mediaServerUrl":"https://archive.org/download/com.hdlive.pro/montest.txt","reportServerUrl":""}
